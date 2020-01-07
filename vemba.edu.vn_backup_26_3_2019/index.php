fbf4bdf91ed0b83a132b994dd30d1f58c7be9203
Kobe Dait <kobedait@hawaii.edu>
2020-01-06 17:06:23 -1000

    Website content

---
+++ vemba.edu.vn_backup_26_3_2019/index.php
@@ -0,0 +1,11 @@
+<!--Content page-->
+<?php include('inc/header.php');?>
+
+<?php
+
+$lang_default == "vi"?include('inc/content-home-vi.php'):include('inc/content-home-en.php')
+
+?>
+
+
+<?php include('inc/footer.php');?>
